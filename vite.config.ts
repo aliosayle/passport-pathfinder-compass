b31c6e504d9763ef6be0df0573c944d109de6c26
aliosayle <aliossaily24@gmail.com>
2025-05-09 13:17:27 +0100

    ngnix configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/passports/', // Ensures assets are loaded correctly from the subfolder
   server: {
     host: "::",
     port: 8080,
